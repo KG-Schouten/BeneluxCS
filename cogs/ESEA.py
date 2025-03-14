a1efecb97be64f3c628845639e9c317f8052519e
kgschouten <kgschouten@tudelft.nl>
2025-03-14 18:06:46 +0100

    Changes to the structure of the imports

--- cogs/ESEA.py
+++ cogs/ESEA.py
@@ -1,10 +1,16 @@
 import nextcord
 from nextcord.ext import commands, tasks
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
+import asyncio
 
+# Module imports
 from faceit_api.faceit_v4 import FaceitData
 from faceit_api.faceit_v1 import FaceitData_v1
+
+from database.db_general import *
+from database.db_down import *
+from database.db_up import *
 
 from data_processing import store_team_data, read_team_data, team_data_path
 from functions import find_team_name, load_api_keys
