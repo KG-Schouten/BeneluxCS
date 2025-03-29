779476544d0ad91d3e80bbeef585187e6a7034f4
KG-Schouten <128721567+KG-Schouten@users.noreply.github.com>
2025-03-29 23:35:39 +0100

    Added functions to gather the player stats from the hub and esea databases

--- database/db_down.py
+++ database/db_down.py
@@ -1,3 +1,8 @@
+# Allow standalone execution
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import mysql.connector
 import json
 import re
@@ -6,67 +11,43 @@ import pandas as pd
 import numpy as np
 from typing import *
 
-from functions import load_api_keys
-from data_processing import process_hub_data
-
 from database.db_up import *
 from database.db_manage import start_database, close_database
 
-# Constants
-db_name = "BeneluxCS"
-
-def start_database():
-    """Starting up the mySQL database so it can be used by the rest of the program"""
-
-    global db_name
-    ## Load password for MySQL database
-    api_keys = load_api_keys()
-    mysql_password = api_keys["MYSQL_PASSWORD"]
-
-    db = mysql.connector.connect(
-        host="localhost",
-        user="Koen",
-        passwd=mysql_password,
-        database=db_name
-    )
-    cursor = db.cursor()
-
-    return db, cursor
-
-def close_database(db, cursor):
-    """Closing the database connection"""
-    db.close()
-    cursor.close()
-
-def gather_hub_stats():
+def gather_hub_stats(days=30):
     """
     Gathers the hub stats from all player over a select time frame (for use in for example the discord bot)
 
     Args:
-        None
+        days (int): Amount of days to gather the data from (default = 30)
+            - Can be 'ALL' in which case all data is being gotten
 
     Returns:
         df
     """
-
+     
     ## Start the database and cursor
     db, cursor = start_database()
 
     ## Set up the timeframe over which to gather the data
-    start_date = (datetime.datetime.now() - datetime.timedelta(days=30)).timestamp()
+    if days == 'ALL':
+        start_date = 0
+    else:
+        start_date = (datetime.now() - timedelta(days=days)).timestamp()
 
-    query = """
+    query = f"""
         SELECT
             p.player_id,
             (
-                SELECT p2.nickname
-                FROM player_statistics p2
+                SELECT p2.player_name
+                FROM hub_player_stats p2
                 WHERE p2.player_id = p.player_id
-                ORDER BY p2.match_id DESC
+                ORDER BY p.match_id, p.match_round DESC
                 LIMIT 1
-            ) AS latest_nickname,
-
-            COUNT(p.match_id) AS matches_played,
+            ) AS last_name,
+            
+            p3.country,
+            COUNT(DISTINCT CONCAT(m.match_id, '-', m.match_round)) AS maps_played,
             SUM(p.result) AS wins,
             ROUND(AVG(p.Kills), 0) AS avg_kills,
             ROUND(AVG(p.Assists), 0) AS avg_assists,
@@ -76,24 +57,29 @@ def gather_hub_stats():
             ROUND(AVG(p.hltv_rating), 2) AS avg_hltv,
             SUM(p.Knife_Kills) AS knife_total,
             SUM(p.Zeus_Kills) AS zeus_total
-        FROM player_statistics p
-        JOIN matches ON matches.match_id = p.match_id
-        WHERE matches.start_time > %s
-        GROUP BY p.player_id
+            
+        FROM hub_player_stats p
+        JOIN hub_maps m ON (m.match_id, m.match_round) = (p.match_id, p.match_round)
+        JOIN hub_matches m2 ON m2.match_id = m.match_id
+        JOIN hub_players p3 ON p3.player_id = p.player_id
+        WHERE m2.match_time > %s
+        GROUP BY p.player_id, last_name
+        ORDER BY p.player_id
     """
 
     # Get player data into dataframe
     cursor.execute(query, (start_date,))
     res = cursor.fetchall()
-    columns = ['player_id','nickname', 'matches_played', 'wins', 'kills', "assists", 'deaths', 'adr','kdr', 'hltv', 'knife_kills', 'zeus_kills']
+    columns = ['player_id','player_name', 'country', 'matches_played', 'wins', 'kills', "assists", 'deaths', 'adr','kdr', 'hltv', 'knife_kills', 'zeus_kills']
     df = pd.DataFrame(res, columns=columns)
 
     # Get total matches played
-    query = """
+    query = f"""
         SELECT
-            COUNT(DISTINCT(matches.match_id)) AS total_matches
-        FROM matches
-        WHERE matches.start_time > %s
+            COUNT(DISTINCT CONCAT(m.match_id, '-', m.match_round)) AS total_matches
+        FROM hub_maps m
+        JOIN hub_matches m2 ON m2.match_id = m.match_id
+        WHERE m2.match_time > %s
     """
     cursor.execute(query, (start_date,))
     res = cursor.fetchall()
@@ -107,24 +93,85 @@ def gather_hub_stats():
     df['wins'] = df['wins'].div(df.matches_played, axis=0).mul(100)
     df['wins'] = df['wins'].astype(float).round(1)
     
-    df = df[df['matches_played'] > 10].reset_index(drop=True)
-    # print(df[df['matches_played'] > 5].sort_values(by='wins', ascending=False).reset_index(drop=True))
-
     return df
 
+def gather_esea_stats(season: int|list) -> pd.DataFrame:
+    """
+    Gathers the hub stats from all player over a select time frame (for use in for example the discord bot)
+
+    Args:
+        season (int | list): The esea season(s) to gather stats from
+
+    Returns:
+        df
+    """
+    
+    # Preparing the sql
+    if isinstance(season, int):
+        seasons = (season,)
+    
+    seasons_sql = ", ".join(map(str, seasons))
+    
+    ## Start the database and cursor
+    db, cursor = start_database()
+
+    query = f"""
+        SELECT
+            p.player_id,
+            (
+                SELECT p2.player_name
+                FROM esea_player_stats p2
+                WHERE p2.player_id = p.player_id
+                ORDER BY p.match_id, p.match_round DESC
+                LIMIT 1
+            ) AS last_name,
+            
+            p3.country,
+            COUNT(DISTINCT CONCAT(m.match_id, '-', m.match_round)) AS maps_played,
+            SUM(p.result) AS wins,
+            ROUND(AVG(p.Kills), 0) AS avg_kills,
+            ROUND(AVG(p.Assists), 0) AS avg_assists,
+            ROUND(AVG(p.Deaths), 0) AS avg_deaths,
+            ROUND(AVG(p.ADR), 1) AS avg_adr,
+            ROUND(AVG(p.K_D_Ratio), 2) AS avg_kdr,
+            ROUND(AVG(p.hltv_rating), 2) AS avg_hltv,
+            SUM(p.Knife_Kills) AS knife_total,
+            SUM(p.Zeus_Kills) AS zeus_total
+            
+        FROM esea_player_stats p
+        JOIN esea_maps m ON (m.match_id, m.match_round) = (p.match_id, p.match_round)
+        JOIN esea_matches m2 ON m2.match_id = m.match_id
+        JOIN esea_players p3 ON p3.player_id = p.player_id
+        JOIN esea_seasons s ON s.season_id = m2.season_id
+        WHERE p3.country IN ('nl', 'be', 'lu') AND s.season_number in ({seasons_sql})
+        GROUP BY p.player_id, last_name
+        ORDER BY p.player_id
+    """
+    
+    cursor.execute(query)
+    res = cursor.fetchall()
+    
+    columns = ['player_id','player_name', 'country', 'matches_played', 'wins', 'kills', "assists", 'deaths', 'adr','kdr', 'hltv', 'knife_kills', 'zeus_kills']
+    df = pd.DataFrame(res, columns=columns)
+    
+    return df
 
 if __name__ == "__main__":
     # Allow standalone execution
     import sys
     import os
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
     
-    df = gather_hub_stats()
-
-    # print(df[df['matches_played'] > 5].sort_values(by='wins', ascending=False).reset_index(drop=True))
-
-    # print(df.nlargest(n=5, columns='matches_played'))
-
-    matches_played = df.nlargest(n=5, columns='matches_played')[['nickname', 'matches_played']]
-    print(matches_played)
+    # db,cursor = start_database()
+    
+    # Gather data for hub
+    df_hub = gather_hub_stats(days=30)
+    df_hub = df_hub.sort_values(by=['hltv'], ascending=False).reset_index(drop=True)
+    
+    # # Gather data for esea
+    # df_esea = gather_esea_stats(season=50)
+    # df_esea = df_esea.sort_values(by=['hltv'], ascending=False).reset_index(drop=True)
+    
+    
+    print(df_hub.to_string())
+    
