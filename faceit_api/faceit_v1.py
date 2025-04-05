2a0c070bbd3629072127027bcb7ca6638575ddc5
KG-Schouten <128721567+KG-Schouten@users.noreply.github.com>
2025-04-05 12:12:47 +0200

    Added v1 and v2 functions for league api

--- faceit_api/faceit_v1.py
+++ faceit_api/faceit_v1.py
@@ -1,38 +1,86 @@
-import json
-import requests
-import urllib.parse
+import aiohttp
+import asyncio
+from asynciolimiter import StrictLimiter
 
-def check_response(res: requests.Response) -> dict | int:
-    if res.status_code == 200:
-        return json.loads(res.content.decode('utf-8'))
-    elif res.status_code ==  400:
-        print("Bad request - The request was unacceptable, often due to missing a required parameter")
-        return res.status_code
-    elif res.status_code ==  401:
-        print("Unauthorized - Invalid or missing credentials")
-        return res.status_code
-    elif res.status_code ==  403:
-        print("Forbidden - The request was understood, but it has been refused or access is not allowed")
-        return res.status_code
-    elif res.status_code ==  404:
-        print("Not Found - The URI requested is invalid or the resource requested, such as a user, does not exist")
-        return res.status_code
-    elif res.status_code ==  429:
-        print("Too Many Requests - Rate limiting has been applied")
-        return res.status_code
-    elif res.status_code ==  503:
-        print("Service Unavailable - The service is temporarily unavailable")
-        return res.status_code
+class RateLimitException(Exception):
+    """ Raised when API rate limit is reached """
+    pass
+
+rate_limiter = StrictLimiter(400, 10)  # 1 request per 0.5 seconds
+
+
+async def check_response(response: aiohttp.ClientResponse) -> dict | int:
+    """ Checks the response from the API and returns the data or raises an exception """
+    if response.status == 200:
+        return await response.json()
+    elif response.status == 429:
+        print("Rate limit reached")
+        raise RateLimitException("Rate limit reached")
+    else:
+        error_map = {
+            400: "Bad Request",
+            401: "Unauthorized",
+            403: "Forbidden",
+            404: "Not Found",
+            500: "Internal Server Error",
+            502: "Bad Gateway",
+            503: "Service Unavailable",
+        }
+        print(error_map.get(response.status, f"HTTP Unknown Error: {response.status}"))
+        return response.status
 
 
 class FaceitData_v1:
     """The Data API for Faceit"""
 
-    def __init__(self):
+    def __init__(self, session: aiohttp.ClientSession):
+        """ Initialize the FaceitData_v1 class"""
 
         self.base_url = 'https://faceit.com/api'
+        self.session = session
+    
+    async def defall_leagues(self) -> dict | int:
+        """ Retrieve all leagues from Faceit """
+        
+        URL = f'{self.base_url}/team-leagues/v2/leagues/a14b8616-45b9-4581-8637-4dfd0b5f6af8/seasons'
+        
+        async with rate_limiter:
+            async with self.session.get(URL) as response:
+                return await check_response(response)
+    
+    async def defleague_season_stages(self, season_id: str) -> dict | int:
+        """
+        Retrieve all stages from a specific league season
 
-    def league_team_details(self, team_id: str) -> dict | int:
+        :param season_id: The ID of the season
+        :return:
+        """
+        
+        URL = "https://www.faceit.com/api/team-leagues/v1/get_filters"
+        
+        body = {"seasonId":season_id}
+        
+        res = requests.post(URL, json=body)
+
+        return check_response(res)
+    
+    async def defleague_season_stage_teams(self, conference_id: str, starting_item_position: int=0, return_items: int=20) -> dict | int:
+        """
+        Retrieve all teams from a specific league season
+
+        :param season_id: The ID of the season
+        :return:
+        """
+        
+        URL = "https://www.faceit.com/api/team-leagues/v2/conferences/{}/registrations?conferenceId={}&search=&offset={}&limit={}".format(
+            conference_id, conference_id, int(starting_item_position), int(return_items)
+        )
+        
+        res = requests.get(URL)
+
+        return check_response(res)
+    
+    async def defleague_team_details(self, team_id: str) -> dict | int:
         """
         Retrieve league details for a team
 
@@ -46,7 +94,7 @@ class FaceitData_v1:
 
         return check_response(res)
     
-    def league_team_matches(self, team_id: str, league_id: str) -> dict | int:
+    async def defleague_team_matches(self, team_id: str, league_id: str) -> dict | int:
         """
         Retrieve league matches for a team
 
@@ -61,7 +109,25 @@ class FaceitData_v1:
 
         return check_response(res)
     
-    def league_team_players(self, team_id: str) -> dict | int:
+    async def defleague_team_details_batch(self, team_ids: list[str]) -> dict | int:
+        """
+        Retrieve league details for a batch of teams
+
+        :param team_ids: The IDs of the teams
+        :return:
+        """
+        
+        URL = "https://www.faceit.com/api/teams/v3/teams/batch-get"
+
+        body = {
+            "ids": team_ids
+        }
+
+        res = requests.post(URL, json=body)
+
+        return check_response(res)
+    
+    async def defleague_team_players(self, team_id: str) -> dict | int:
         """
         Retrieve active league members from a team (so in the ESEA roster
 
@@ -75,11 +141,13 @@ class FaceitData_v1:
 
         return check_response(res)
 
-    def player_friend_list(self, player_id: str, starting_item_position: 0=int, return_items: 20=int) -> dict | int:
+    async def defplayer_friend_list(self, player_id: str, starting_item_position: int=0, return_items: int=20) -> dict | int:
         """
         Retrieve the friend list from a specific player
         
         : param player_id: The ID of the player
+        :param starting_item_position: The starting item position (default is 0)
+        :param return_items: The number of items to return (default is 20)
         :return:
         """
 
@@ -90,5 +158,24 @@ class FaceitData_v1:
         res = requests.get(URL)
         
         return check_response(res)
+    
+    async def defplayer_hubs(self, player_id: str, return_items: int=10, starting_item_position: int=0) -> dict | int:
+        """
+        Retrieve the hubs from a specific player
+        
+        :pram player_id: The ID of the player
+        :param return_items: The number of items to return (default is 10)
+        :param starting_item_position: The starting item position (default is 0)
+        :return:
+        """
+        
+        URL = "{}/hubs/v1/user/{}/membership?limit={}&offset={}&clubs=true".format(
+            self.base_url, player_id, return_items, starting_item_position
+        )
+        
+        res = requests.get(URL)
+        
+        return check_response(res)
+
 
     
