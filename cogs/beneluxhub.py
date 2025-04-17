af5be3b358ecdc8fcc898ce2b8e2640a12314f86
KG-Schouten <128721567+KG-Schouten@users.noreply.github.com>
2025-04-18 00:40:18 +0200

    First work on leaderboard function

--- cogs/beneluxhub.py
+++ cogs/beneluxhub.py
@@ -3,6 +3,7 @@ from nextcord.ext import commands, tasks
 import json
 from datetime import datetime, timedelta
 import asyncio
+from itertools import batched
 
 # Module imports
 from faceit_api.faceit_v4 import FaceitData
@@ -18,12 +19,33 @@ from functions import find_team_name, load_api_keys
 # Initialize the API keys
 api_keys = load_api_keys()
 
-def format_leaderboard(df, title):
-    i = 0
-    leaderboard_str = f"**{title}**\n"
+def create_leaderboard(embed, df):
+    # Initializing values
+    space = "\u1CBC"
+    idx_emojis = [":first_place:", ":second_place:", ":third_place:"]
+       
+    # Make the strings for the title field and the three inline fields for this leaderboard (place, nickname, stat)
+    title = df.index.name
+    title_str = f"**{title}**"
+    
+    idx_str = ""
+    name_str = ""
+    stat_str = ""
     for i, row in df.iterrows():
-        leaderboard_str += f"{i+1}. {row.iloc[0]} - {row.iloc[1]}\n"
-    return leaderboard_str
+        if i+1 <= len(idx_emojis):
+            idx_str += f"{idx_emojis[i]}\n"
+        else:
+            idx_str += f"{i+1}.\n"
+        name_str += f"{row.iloc[0]}\n"
+        stat_str += f"{row.iloc[1]}\n"
+    
+    embed.add_field(name="", value="", inline=False)
+    embed.add_field(name=title_str, value="", inline=False) # Title field
+    embed.add_field(name="", value=idx_str, inline=True) # Index field
+    embed.add_field(name="", value=name_str, inline=True) # Index field
+    embed.add_field(name="", value=stat_str, inline=True) # Index field
+    
+    return embed
 
 class BeneluxHub(commands.Cog):
     def __init__(self, client):
@@ -39,12 +61,19 @@ class BeneluxHub(commands.Cog):
         df = gather_hub_stats()
         df['knife_kills'] = pd.to_numeric(df['knife_kills'], errors='coerce')
 
+        n = 5
         # Getting the top 5's for certain columns
-        matches_played = df.nlargest(n=3, columns='matches_played')[['nickname', 'matches_played']].reset_index(drop=True)
-        winrate = df.nlargest(n=3, columns='wins')[['nickname', 'wins']].reset_index(drop=True)
-        hltv = df.nlargest(n=3, columns='hltv')[['nickname', 'hltv']].reset_index(drop=True)
-        knifes = df.nlargest(n=3, columns='knife_kills')[['nickname', 'knife_kills']].reset_index(drop=True)
-
+        matches_played = df.nlargest(n=n, columns='matches_played')[['nickname', 'matches_played']].reset_index(drop=True)
+        matches_played.index.name = 'Matches Played'
+        winrate = df.nlargest(n=n, columns='wins')[['nickname', 'wins']].reset_index(drop=True)
+        winrate.index.name = 'Winrate'
+        hltv = df.nlargest(n=n, columns='hltv')[['nickname', 'hltv']].reset_index(drop=True)
+        hltv.index.name = 'HLTV Rating'
+        knifes = df.nlargest(n=n, columns='knife_kills')[['nickname', 'knife_kills']].reset_index(drop=True)
+        knifes.index.name = 'Knife Kills'
+        
+        df_list = [matches_played, winrate, hltv, knifes]
+        
         # embed creating
         embed = nextcord.Embed(
             title="BeneluxHub Monthly Stats",
@@ -53,11 +82,9 @@ class BeneluxHub(commands.Cog):
 
         embed.set_author(name="Benelux Bot",
                          icon_url="https://pbs.twimg.com/profile_images/1796692667008507904/sU3cv9vT_400x400.jpg")
-        
-        embed.add_field(name="", value=format_leaderboard(matches_played, "Matches Played"), inline=False)
-        embed.add_field(name="", value=format_leaderboard(winrate, "Winrate"), inline=False)
-        embed.add_field(name="", value=format_leaderboard(hltv, "HLTV Rating"), inline=False)
-        embed.add_field(name="", value=format_leaderboard(knifes, "Knife Kills"), inline=False)
+    
+        for df in df_list:
+            embed = create_leaderboard(embed, df)
 
         await ctx.send(embed=embed)
 
