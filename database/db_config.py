b29820268dbf04e696a84e2c2ee7e49d197e8284
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-05-04 00:18:24 +0200

    New table names

--- database/db_config.py
+++ database/db_config.py
@@ -1,59 +1,40 @@
-db_name = "BeneluxCS"
+db_name = "railway"
 
-# Dictionaries for the table names and their useful values connected to it
-table_names_esea = { # 'table_name' : create_table_query args (primary_keys, foreign_keys)
-    "esea_teams": [
-        ['team_id']
-    ], 
-    "esea_seasons": [
-        ['season_id']
-    ], 
-    "esea_stages": [
-        ['stage_id'], 
-        [('season_id', 'esea_seasons')]
-    ], 
-    "esea_players": [
-        ['player_id']
-    ], 
-    "esea_player_team_seasons": [
-        ['player_id', 'team_id', 'season_id'], 
-        [('player_id', 'esea_players'), ('team_id', 'esea_teams'), ('season_id', 'esea_seasons')]
-    ], 
-    "esea_matches": [
-        ['match_id']
+table_names = {
+    "seasons": [
+        ['event_id'],
     ],
-    "esea_maps": [
-        ['match_id', 'match_round'], 
-        [('match_id', 'esea_matches')]
+    "teams_benelux": [
+        ['team_id', 'region_id'],
     ],
-    "esea_team_maps": [
-        ['team_id', 'match_id', 'match_round'], 
-        [('team_id', 'esea_teams'), (('match_id', 'match_round'), 'esea_maps')]
+    "events": [
+        ['internal_event_id']
     ],
-    "esea_player_stats": [
-        ['player_id', 'match_id', 'match_round'], 
-        [('player_id', 'esea_players'), ('team_id', 'esea_teams'), (('match_id', 'match_round'), 'esea_maps')]
-    ]
-}
-
-table_names_hub = {
-    "hub_matches": [
-        ["match_id"]
+    "matches": [
+        ['match_id'],
+        [('internal_event_id', 'events')]
     ],
-    "hub_maps": [
+    "maps": [
         ['match_id', 'match_round'],
-        [('match_id', 'hub_matches')]
+        [('match_id', 'matches')]
     ],
-    "hub_team_maps": [
-        ['team_id', 'match_id', 'match_round'],
-        [(('match_id', 'match_round'), 'hub_maps')]
-    ],
-    "hub_players": [
+    "players": [
         ['player_id']
-    ], 
-    "hub_player_stats": [
-        ["player_id", "match_id", "match_round"], 
-        [('player_id', 'hub_players'), (('match_id', 'match_round'), 'hub_maps')]
+    ],
+    "teams": [
+        ['team_id']
+    ],
+    "players_stats": [
+        ['player_id', 'match_id', 'match_round'],
+        [('player_id', 'players'), ('team_id', 'teams'), (('match_id', 'match_round'), 'maps')]
+    ],
+    "teams_matches": [
+        ['match_id', 'team_id'],
+        [('team_id', 'teams'), ('match_id', 'matches')]
+    ],
+    "teams_maps": [
+        ['team_id', 'match_id', 'match_round'],
+        [(('match_id', 'match_round'), 'maps'), ('team_id', 'teams')]
     ]
 }
 
@@ -62,7 +43,7 @@ VALID_PG_TYPES = {
     "REAL", "DOUBLE PRECISION", "NUMERIC",
     "SERIAL", "BIGSERIAL",
     "BOOLEAN",
-    "CHAR", "VARCHAR", "TEXT",
+    "CHAR", "VARCHAR", "TEXT", "TEXT[]",
     "DATE", "TIMESTAMP", "TIME",
     "UUID",
     "BYTEA"
