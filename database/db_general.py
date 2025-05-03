b1c33efe82a873483dda729f5374cae452e13b0e
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-05-04 00:18:46 +0200

    Reworked general functions

--- database/db_general.py
+++ database/db_general.py
@@ -3,10 +3,15 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+import asyncio
+import psycopg2
+
 from database.db_down import *
 from database.db_up import *
 from database.db_manage import start_database, close_database
-from database.db_config import table_names_esea, table_names_hub, VALID_PG_TYPES
+from database.db_config import *
+
+from data_processing.dp_events import process_esea_teams_data
 
 def delete_table(table_names: str | list, confirm: bool = True):
     """
@@ -41,16 +46,16 @@ def delete_table(table_names: str | list, confirm: bool = True):
             # Disable foreign key checks temporarily
             cursor.execute("SET session_replication_role = 'replica';")
             
-            # Retrieve all table names in the database
+            # Retrieve all table names
             cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
-            tables = cursor.fetchall()
+            tables = [table[0] for table in cursor.fetchall()]
 
-            if tables == []: # Check if there are tables in the database
+            if not tables:
                 print("No tables found in the database.")
                 return
             
             if table_names == "ALL": # If "ALL" is specified, drop all tables
-                tables_to_drop = [table[0] for table in tables]
+                tables_to_drop = tables
             elif isinstance(table_names, str): # If a single table name is specified, drop that table
                 tables_to_drop = [table_names]
             elif isinstance(table_names, list): # If a list of table names is specified, drop those tables
@@ -60,12 +65,12 @@ def delete_table(table_names: str | list, confirm: bool = True):
                 return
 
             for table in tables_to_drop:
-                # Check if the table exists in the database
-                if table in tables:
-                    cursor.execute(f"DROP TABLE IF EXISTS {table};")
-                    print(f"Dropped table: {table}")
-                else:
-                    print(f"Table {table} does not exist in the database. Continuing...")
+                print(f"Dropping table: {table}")
+                try:
+                    cursor.execute(f"DROP TABLE IF EXISTS {table} CASCADE;")
+                except psycopg2.Error as e:
+                    print(f"Error dropping table {table}: {e}")
+                    db.rollback() # Rollback the failed command, continue
 
         finally:
             # Enable the foreign key checks again
@@ -73,6 +78,65 @@ def delete_table(table_names: str | list, confirm: bool = True):
             db.commit()
             close_database(db, cursor)
 
+def create_tables():
+    
+    ## Gather esea data for one team in one season
+    season_number = 52
+    results = asyncio.run(process_esea_teams_data(season_number=season_number, team_id="6bb0008b-e18f-4723-a265-cc6f8538efe4")) # takes the data from myth in intermediate season 52
+    if results:
+        df_seasons, df_events, df_teams_benelux, df_matches, df_teams_matches, df_teams, df_maps, df_teams_maps, df_players_stats, df_players = results
+    else:
+        print("No data found for the specified team and season.")
+        return
+    
+    # Create the table_columns dictionary for each dataframe
+    for table_name in table_names.keys():
+        df = eval(f"df_{table_name}")
+        if df is None:
+            print(f"Dataframe {table_name} is None. Skipping...")
+            continue
+        table_columns = create_table_columns(df)
+        primary_keys = table_names[table_name][0]
+        if len(table_names[table_name]) > 1:
+            foreign_keys = table_names[table_name][1]
+        else:
+            foreign_keys = []
+        
+        # Create the table in the database
+        create_table(table_name=table_name, table_columns=table_columns, primary_keys=primary_keys, foreign_keys=foreign_keys)
+    
+def create_table_columns(df: pd.DataFrame) -> dict:
+    """
+    Creating the table_columns dictionary used in the create_table function for a specific dataframe
+    """
+    
+    # List of column names
+    column_names = df.columns.tolist()
+    
+    # List of column values (first row expect if value is None, use the first non-null value)
+    # Get largest or longest value for each column
+    column_values = []
+    for column_name in column_names:
+        if isinstance(df[column_name].iloc[0], str):
+            # Get longest string in the column
+            column_values.append(df[column_name].loc[df[column_name].str.len().idxmax()])
+            
+        elif isinstance(df[column_name].iloc[0], (int, float)):
+            # Get largest number in the column
+            column_values.append(df[column_name].loc[df[column_name].idxmax()])
+        else:
+            # Get first non-null value in the column
+            column_values.append(df[column_name].loc[df[column_name].first_valid_index()])
+    
+    # List of column types
+    column_types = [determine_column_type(column_value) for column_value in column_values] 
+    
+    table_columns = {
+        column_name: column_type for column_name, column_type in zip(column_names, column_types)
+    }
+    
+    return table_columns
+    
 def create_table(table_name: str, table_columns: dict = {}, primary_keys: list = [], foreign_keys: list = []):
     """
     Creating the tables in the Postgresql database
@@ -105,9 +169,8 @@ def create_table(table_name: str, table_columns: dict = {}, primary_keys: list =
 
         db, cursor = start_database()
         cursor.execute(query)
-
     except Exception as e:
-        print(f"Error while starting database: {e}")
+        print(f"Error while creating table {table_name}: {e}")
         return
     finally:
         db.commit()
@@ -142,7 +205,7 @@ def create_table_query(table_name: str, table_columns: dict = {}, primary_keys: 
         return
 
     # Create the table column types list
-    column_types = []
+    column_definitions = []
     for col_name, col in table_columns.items():
         base_type = col.split("(")[0].upper()
         if base_type not in VALID_PG_TYPES:
@@ -150,7 +213,7 @@ def create_table_query(table_name: str, table_columns: dict = {}, primary_keys: 
             table_columns[col_name] = column_type
         else:
             column_type = col
-        column_types.append(column_type)     
+        column_definitions.append(f"{col_name} {column_type}")     
 
     # Create the table column keys list # ADD SOME TYPE CHECKS HERE
     table_keys = []
@@ -175,25 +238,29 @@ def create_table_query(table_name: str, table_columns: dict = {}, primary_keys: 
     # Create the table query
     table_query = f"""
     CREATE TABLE IF NOT EXISTS {table_name} (
-        {", ".join(column_types)}, {", ".join(table_keys)}
+        {", ".join(column_definitions + table_keys)}
     );
     """
     # print(table_query) # Uncomment to see the query
     return table_query
 
-def determine_column_type(column_type) -> str:
+def determine_column_type(column_value) -> str:
     """ Determines the column type based on the input example value"""
-    if isinstance(column_type, float):
+    if isinstance(column_value, float):
         return "FLOAT"
-    elif isinstance(column_type, int):
-        if column_type > 2147483647:
+    elif isinstance(column_value, int):
+        if column_value > 2147483647:
             return "BIGINT"
         else:
             return "INT"
-    elif isinstance(column_type, str) and len(column_type) == 36 and '-' in column_type:
-        return "VARCHAR(50)"
-    elif isinstance(column_type, str) and column_type.isdigit():
+    elif isinstance(column_value, str) and len(column_value) == 36 and '-' in column_value:
+        return "VARCHAR(100)"
+    elif isinstance(column_value, str) and column_value.isdigit():
         return "BIGINT"
+    elif isinstance(column_value, str) and len(column_value) > 255:
+        return "TEXT"
+    elif isinstance(column_value, list or tuple):
+        return "TEXT[]"
     else:
         return "VARCHAR(255)" # Default to VARCHAR
 
@@ -203,6 +270,8 @@ if __name__ == "__main__":
     import os
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
     
-    delete_table()
+    delete_table("ALL", confirm=True) # Delete all tables in the database
+    create_tables()
+    # update_esea_data(update_type="ALL")
 
 
