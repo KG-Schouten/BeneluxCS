cc81a86615ba0957d919828f3312a0145b6b684c
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-05-08 18:04:17 +0200

    Added update.py for cron job in railway to update database with esea and hub data

---
+++ database/update.py
@@ -0,0 +1,8 @@
+## This file will be run regularly to update the database with the new data
+
+from db_up import update_data
+
+hub_id = "801f7e0c-1064-4dd1-a960-b2f54f8b5193"
+
+update_data("new", "hub", event_id=hub_id)
+update_data("new", "esea")
