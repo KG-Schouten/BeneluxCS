8e4dcc3f21ada9c4d2090a624b497df28bb43582
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-07-12 01:38:09 +0200

    Improved column filtering logic for stats page and small changes to logic for esea page

--- BeneluxWebb/website/views.py
+++ BeneluxWebb/website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+import re
 
 views = Blueprint('views', __name__, template_folder='../templates')
 
@@ -39,11 +40,36 @@ def stats():
         search = request.args.get('search', '').strip()
         page = max(1, request.args.get('page', type=int, default=1))
         per_page = request.args.get('per_page', type=int, default=25)
-        
+                        
+        data, stat_field_names = gather_player_stats_esea(
+            countries=countries,
+            seasons=season_numbers,
+            divisions=division_names,
+            stages=stage_names,
+            timestamp_start=0,
+            timestamp_end=0,
+            team_ids=[],
+            search_player_name=search
+        )
+            
         # column handling
-        columns_perm = ['adr', 'k d ratio', 'hltv']
-        columns_str = request.args.get('columns')
-        columns = columns_str.split(',') if columns_str else []
+        columns_perm = ['adr', 'k_r_ratio', 'k_d_ratio', 'headshots_percent', 'hltv']
+        columns_mapping = {
+            'adr':                  {'name': 'ADR',         'round': 0},
+            'k_r_ratio':            {'name': 'K/R',         'round': 2},
+            'k_d_ratio':            {'name': 'K/D',         'round': 2, 'good': 1.1, 'bad': 0.9},
+            'headshots_percent':    {'name': 'HS %',        'round': 0},
+            'hltv':                 {'name': 'HLTV 1.0',    'round': 2, 'good': 1.1, 'bad': 0.9},
+        }
+        for col in stat_field_names:
+            if col not in columns_mapping:
+                columns_mapping[col] = {'name': re.sub(r'^_', '', col).replace('_', ' ').title(), 'round': 2}
+        
+        columns_filter_str = request.args.get('columns')
+        columns_filter = columns_filter_str.split(',') if columns_filter_str else []
+        
+        unique_columns = set(stat_field_names)
+        columns_filter = [c for c in columns_filter if c in unique_columns and c not in columns_perm]
         
         # Sort parameter handling
         sort = request.args.get('sort')
@@ -56,24 +82,6 @@ def stats():
             else:
                 sort_key = sort
                 
-        data, stat_field_names = gather_player_stats_esea(
-            countries=countries,
-            seasons=season_numbers,
-            divisions=division_names,
-            stages=stage_names,
-            timestamp_start=0,
-            timestamp_end=0,
-            team_ids=[],
-            search_player_name=search
-        )
-
-        # Filter columns if specified
-        valid_columns = set(stat_field_names)
-        columns = [c for c in columns if c in valid_columns and c not in columns_perm]
-        if not columns:
-            columns = []
-        columns_final = columns_perm + columns
-        
         # Sort data
         if sort_key:
             if sort_key in stat_field_names:
@@ -99,9 +107,10 @@ def stats():
         if is_ajax:
             html = render_template("partials/_stats_table.html",
                 data=paginated_data,
-                stat_field_names=columns,
-                columns_perm=columns_perm,
-                display_columns=columns_final,
+                stat_field_names=stat_field_names, # All stat field names (original name)
+                columns_perm=columns_perm, # Permanent columns (original name)
+                columns_filter=columns_filter, # Filtered columns (original name)
+                columns_mapping=columns_mapping, # Mapping for columns (original name to display name)
                 page=page,
                 per_page=per_page,
                 total=total,
@@ -114,9 +123,10 @@ def stats():
         return render_template(
             "stats.html",
             data=paginated_data,
-            stat_field_names=stat_field_names,
-            columns_perm=columns_perm,
-            display_columns=columns_final,
+            stat_field_names=stat_field_names, # All stat field names (original name)
+            columns_perm=columns_perm, # Permanent columns (original name)
+            columns_filter=columns_filter, # Filtered columns (original name)
+            columns_mapping=columns_mapping, # Mapping for columns (original name to display name)
             search=search,
             page=page,
             total=total,
@@ -147,7 +157,8 @@ def stats():
             data=[],
             stat_field_names=[],
             columns_perm=[],
-            display_columns=[],
+            columns_filter=[],
+            columns_mapping=[],
             search='',
             page=1,
             total=0,
@@ -161,9 +172,13 @@ def stats():
 
 @views.route('/esea')
 def esea():
-	from database.db_down import gather_esea_season_numbers
-	season_numbers = gather_esea_season_numbers()
-	return render_template("esea.html", season_numbers=season_numbers)
+    # Current time in unix
+    from datetime import datetime
+    current_time = int(datetime.now().timestamp())
+    
+    from database.db_down import gather_esea_season_info    
+    season_info = gather_esea_season_info()
+    return render_template('esea.html', season_info=season_info, current_time=current_time)
 
 @views.route('/esea/season/<int:season_number>')
 def esea_season_partial(season_number):
@@ -172,7 +187,6 @@ def esea_season_partial(season_number):
     
     # Extract teams for the specified season
     divisions = esea_data.get(season_number, {})
-    
     return render_template('partials/_esea_season.html', divisions=divisions, season=season_number)
 
 @views.route('/leaderboard')
