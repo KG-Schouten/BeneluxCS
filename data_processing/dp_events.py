8a16e20500a985d9c2b5a740b3ac3018d36dccd9
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-07-15 13:56:50 +0200

    JSON file loading fix

--- data_processing/dp_events.py
+++ data_processing/dp_events.py
@@ -315,11 +315,11 @@ def read_league_teams_json(
         pd.DataFrame: DataFrame with team details.
     """
     # Data path
-    BASE_DIR = os.path.dirname(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
-    URL = os.path.join(BASE_DIR, 'data_processing/data', 'league_teams.json')
+    PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    DATA_PATH = os.path.join(PROJECT_ROOT, 'data_processing', 'data', 'league_teams.json')
 
     try:
-        with open(URL, 'r', encoding='utf-8') as f:  # Open the file in read mode with UTF-8 encoding
+        with open(DATA_PATH, 'r', encoding='utf-8') as f:  # Open the file in read mode with UTF-8 encoding
             data = json.load(f)  # Load the JSON content into a Python dictionary
             
         # Convert into a dataframe
