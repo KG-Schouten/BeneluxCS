92462e69d55047407f14da07d0bdfe18bef26454
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-10-18 23:52:44 +0200

    Minor change

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -3,6 +3,6 @@ from BeneluxWebb.website.scheduler import init_scheduler
 from BeneluxWebb.website.update_logger import log_message
 
 def when_ready(server):
-    log_message("scheduler", "Gunicorn master is ready — initializing scheduler once", "info")
+    log_message("scheduler", "Gunicorn master is ready - initializing scheduler once", "info")
     app = create_app()
     init_scheduler(app)
