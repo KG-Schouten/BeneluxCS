e2cd1dd92ba12caa60d6cab9c2001731a66be560
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-10-18 23:12:20 +0200

    Removed tests

--- data_processing/tests/test_update/test_update_esea/test_update_esea_seasons_events.py
+++
@@ -1,9 +0,0 @@
-# Allow standalone execution
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
-import pytest
-import pandas as pd
-from datetime import datetime, timezone
-from unittest.mock import patch
