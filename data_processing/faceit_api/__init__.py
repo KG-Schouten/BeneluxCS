885c07dcdd517bcfaee037c102b4103476076944
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-10-21 13:53:15 +0200

    Minor fix

--- data_processing/faceit_api/__init__.py
+++ data_processing/faceit_api/__init__.py
@@ -1,7 +1,6 @@
 from . import async_progress
 from . import faceit_v1
 from . import faceit_v4
-from . import logging_config
 from . import response_handler
 from . import sliding_window
 
@@ -9,7 +8,6 @@ __all__ = [
     'async_progress',
     'faceit_v1',
     'faceit_v4',
-    'logging_config',
     'response_handler',
     'sliding_window'
 ]
