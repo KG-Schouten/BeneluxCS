b38c9769b29fd1a0f9599177dc80ac3ddae6944f
kgschouten <128721567+KG-Schouten@users.noreply.github.com>
2025-10-24 17:03:43 +0200

    Updated website_main.py

--- website_main.py
+++ website_main.py
@@ -1,6 +1,11 @@
+import eventlet
+eventlet.monkey_patch()
+
 from BeneluxWebb.website import create_app
+from flask_socketio import SocketIO
 
 app = create_app()
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
